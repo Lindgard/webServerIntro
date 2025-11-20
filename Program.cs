d6c4957caa22493571d3031997476dfa2b41a63e
ALP <a.b.lindgard@gmail.com>
2025-11-20 11:04:30 +0100

    adding examples of simple functionality

--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var visits = 0;
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/testing", () => {
+    visits++;
+    if (visits > 10)
+    {
+        return "sorry, we're full today";
+    } else {
+    return "This is a test";
+    }
+});
+
+app.MapGet("/crash", () =>
+{
+    visits++;
+    return Results.InternalServerError();
+});
+
+app.MapGet("/visits", () =>
+{
+    visits++;
+    return $"Total requests responded to: {visits}";
+});
 
 app.Run();
